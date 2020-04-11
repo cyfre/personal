51790c4b467b9d5eb050e872b235b92d6f53342c
cfreshman <cyruswfreshman@gmail.com>
2020-04-10 23:03:40 -0400

    resolving issue with deployed server

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/api/graffiti', require('./graffiti').routes);
 
 // production build
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
