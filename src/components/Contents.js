9817fa1000f932d98d53287d0efb9820945135dc
Cyrus Freshman <cyruswfreshman@gmail.com>
2020-06-30 09:58:07 -0400

    embedded pages: added focus on iframe on page load (to fix /snackman)

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -32,8 +32,12 @@ const Page = () => {
 
 const Embedded = ({ name }) => {
     let src = `/project/${name}/index.html`;
+    
+    useEffect(() => document.getElementById('embedded').focus(), []);
+
     return (
         <iframe
+            id="embedded"
             title={name}
             allowFullScreen
             frameBorder="0"
