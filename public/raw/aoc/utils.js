f01dab7f5b1cdeda241a75a18dfae8d9c5ea2c7d
cfreshman <cyruswfreshman@gmail.com>
2020-12-07 02:10:06 -0500

    /aoc day 7 2020

--- public/raw/aoc/utils.js
+++ public/raw/aoc/utils.js
@@ -1,7 +1,22 @@
 (() => {
-    window.U = {
+    const U = {
+        opt: (val, func) => func ? func(val) : val,
+        o: (field, value) => ({ [field]: value }),
+        k: (obj, func) => U.opt(Object.keys(obj), func),
+        v: (obj, func) => U.opt(Object.values(obj), func),
+        e: (obj, func) => U.opt(Object.entries(obj), func),
+        merge: objs => Object.assign({}, ...objs),
+        map: (obj, func) => Object.entries(obj).map(entry => func(...entry)),
         numSort: arr => arr.sort((a, b) => a - b),
-        sum: (arr, func) => arr.reduce((sum, val) => sum + func(val), 0),
-        product: (arr, func) => arr.reduce((prod, val) => prod * func(val), 1),
+        sum: (arr, func) => arr.reduce((sum, val) => sum + U.opt(val, func), 0),
+        product: (arr, func) => arr.reduce((prod, val) => prod * U.opt(val, func), 1),
+        match: (strs, regex, func) => strs.map(str => U.opt(str.match(regex), func)),
+        union: (a, b) => new Set(...a, ...b),
+        answer: (input, func) => {
+            let answers = {};
+            func(input.split('\n'), ...['p1', 'p2'].map(pN => aN => { answers[pN] = aN; }));
+            return answers;
+        },
     };
+    window.U = U;
 })();
